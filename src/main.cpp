35404e167578b224598fda37c92cb68cb901bc71
Giancarlo Ferretto <81869512+Giancarlo-Ferretto@users.noreply.github.com>
2022-03-26 17:07:15 -0300

    algunos comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// Problema del ladrón.
+// Problema del ladrón y la mochila.
 
 #include <iostream>
 #include <list>
@@ -8,30 +8,30 @@ using namespace std;
 
 #define MAX_BAG_WEIGHT  15
 
-class Item {
+class Item { //los items a tomar con su peso y valor
 public:
     int weight, price;
 };
 
-class Action {
+class Action { //la acción es un item a tomar
 public:
     Item item;
 };
 
-class State {
+class State { //el estado, la mochila con sus items y peso actual
 public:
     list<Action> items;
     int weight;
 };
 
-State transition(State& state, Action& action) {
+State transition(State& state, Action& action) { //transición, se le agrega una acción al estado / toma un item
     State new_state = state;
     new_state.items.push_back(action);
     new_state.weight += action.item.weight;
     return new_state;
 }
 
-int is_valid_action(State& state) {
+int is_valid_action(State& state) { //la acción es válida si el peso de la mochila no supera al máximo
     return (state.weight <= MAX_BAG_WEIGHT);
 }
 
@@ -48,7 +48,7 @@ list<Action> get_actions(State& state, Item items[]) {
     list<Action> actions;
     //printf("[%p] ", state);
     for(int i = 0; i < 10; i++) {
-        if(!is_state_has_action_item(state, items[i])) {
+        if(!is_state_has_action_item(state, items[i])) { //si el item no ha sido guardado en la mochila lo toma
             state.weight += items[i].weight;
             if(is_valid_action(state)) {
                 //printf("disponible item %d$ %dkg ", items[i].price, items[i].weight);
@@ -67,20 +67,18 @@ list<Action> get_actions(State& state, Item items[]) {
 int breadth_first_search(State& initial_state, Item items[]) {
     queue<State> bfs_queue;
     bfs_queue.push(initial_state);
-
     while(!bfs_queue.empty()) {
         State new_state = bfs_queue.front();
         bfs_queue.pop();
 
         list<Action> actions = get_actions(new_state, items);
-
-        if(actions.empty()) { //is_final_state?
+        if(actions.empty()) { //is_final_state? encuentra el primer estado final y lo retorna
             for(Action action : new_state.items) {
                 printf("item guardado %d$ %dkg \n", action.item.price, action.item.weight);
             }
             return new_state.items.size();
         }
-        
+    
         for(Action action : actions) {
             State ss = transition(new_state, action);
             //printf("items guardados estado %p: %d \n", ss, ss.items.size()); 
@@ -91,7 +89,7 @@ int breadth_first_search(State& initial_state, Item items[]) {
 }
 
 int main() {
-    Item test_items[10];
+    Item test_items[10]; //listado de items disponibles para tomar
     test_items[0].weight = 1;
     test_items[0].price = 1;
     test_items[1].weight = 2;
@@ -113,10 +111,10 @@ int main() {
     test_items[9].weight = 9;
     test_items[9].price = 8;
 
-    State initial_state;
+    State initial_state; //mochila inicial y vacía
     initial_state.weight = 0;
 
-    int steps = breadth_first_search(initial_state, test_items);
+    int steps = breadth_first_search(initial_state, test_items); //algoritmo bfs
     printf("numero de items guardados: %d\n", steps);
     return 0;
 }
